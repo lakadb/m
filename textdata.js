a7a3c2c527a5a07de7bcecf46b5b39bc408f4ecf
lakadb <lakabotdb@gmail.com>
2025-07-24 21:27:35 +0530

    Update textdata.js

--- textdata.js
+++ textdata.js
@@ -1,10 +1,10 @@
 {
-"footer": "Follow us : whatsapp.com/channel/0029Vb6FEaJ5kg79LXF2gG3Z\n\n> *𝗣 𝗢 𝗪 𝗘 𝗥 𝗘 𝗗  𝗕 𝗬   𝗟 𝗔 𝗞 𝗔-𝗠 𝗗 🦅*",
+"footer": "> *𝗣 𝗢 𝗪 𝗘 𝗥 𝗘 𝗗  𝗕 𝗬   𝗟 𝗔 𝗞 𝗔-𝗠 𝗗 🦅*",
 "mainimg": "https://raw.githubusercontent.com/lakadb/m/refs/heads/main/new.jpg",
 "version": "3.0.0",
 "mongo": "mongodb+srv://wtf:wtf@wtf.mz4qz04.mongodb.net/wtf",
-"repoo": "github.com/mrlaka/Laka-Md-v3",
-"zipfile": "https://api.github.com/repos/MrLaka/Laka-Md-V3/zipball",
+"repoo": "github.com/lakaofc1/Laka-Md",
+"zipfile": "https://api.github.com/repos/lakaofc1/Laka-Md/zipball",
 "weburl": "lakamd-web-e5d99ab166f3.herokuapp.com/pair",
 "barkmdg": "අපිට කොහෙද මාලු අපි යන්නේ නැනේ මුහුදු",
 "alivemg": "*Laka Md WhatsApp Bot Is SriLanka Creacte By Lashmitha (LakaOfc) 🔥*",
